7986ce19a17401510048bff4825cadb50aa4a3f5
M0BIUS1 <mahinay.franciskyle@gmail.com>
2025-10-06 21:25:25 +0800

    validation done  but the data needs fix

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),          # root URL
+    path('', views.home_view, name='home'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('studentDashboard/', views.studentDashboard, name='studentDashboard'),
 ]
