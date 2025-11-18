a2efcfa1498dd726e1d1d347ecc933315b62f6d0
unknown <niccomalds@gmail.com>
2025-11-19 01:26:58 +0800

    Fix missing application views and URLs

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('studentDashboard/', views.studentDashboard, name='studentDashboard'),
     path('organizationDashboard/', views.organizationDashboard, name='organization_dashboard'),
+    path('my-applications/', views.my_applications, name='my_applications'),
+    path('create-application/<int:posting_id>/', views.create_application, name='create_application'),
+    path('withdraw-application/<int:application_id>/', views.withdraw_application, name='withdraw_application'),
+    path('profile/', views.profile_view, name='profile'),
 ]   
